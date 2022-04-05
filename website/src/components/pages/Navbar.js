5ffff050fc1f60ef7e6ceeb00577666dfa4dd353
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-05 11:49:11 +0500

    Added State Variables

--- website/src/components/pages/Navbar.js
+++ website/src/components/pages/Navbar.js
@@ -6,7 +6,19 @@ import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960 ) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  };
+
+  window.addEventListener('resize', showButton);
 
   return (
       <>
