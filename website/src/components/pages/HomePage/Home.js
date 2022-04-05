15f7a09148c1de05162242efb99d8567a602869f
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-05 22:39:40 +0500

    Pricing Plans Working

--- website/src/components/pages/HomePage/Home.js
+++ website/src/components/pages/HomePage/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import HeroSection from '../HeroSection';
+import ServicesList from '../ServicesList';
 import { homeObjOne, homeObjThree, homeObjTwo, homeObjFour } from './Data';
 
 function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
+      <ServicesList />
       <HeroSection {...homeObjTwo} />
       <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjFour} />
+      
     </>
   );
 }
