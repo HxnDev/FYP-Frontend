432540d12ae8de163a3268f0d00d6a8b721b769f
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-05 22:20:15 +0500

    Working webiste

--- website/src/components/pages/HomePage/Home.js
+++ website/src/components/pages/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HeroSection from '../HeroSection';
-import { homeObjOne } from './Data';
+import { homeObjOne, homeObjThree, homeObjTwo, homeObjFour } from './Data';
 
 function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjFour} />
     </>
   );
 }
