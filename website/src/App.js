6f654483d40e7c76e2b37273aa25c46f11b5ec44
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-05 23:37:40 +0500

    Services Seperate Page Created

--- website/src/App.js
+++ website/src/App.js
@@ -4,15 +4,19 @@ import Home from './components/pages/HomePage/Home';
 import HeroSection from './components/pages/HeroSection';
 import Navbar from './components/pages/Navbar';
 import Footer from './components/pages/Footer/Footer';
+import Services from './components/pages/Services/Services';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Home />
-      <Routes>
-        <Route path='/home' exact component={HeroSection} />
-      </Routes>
+      <Services />
+      {/* <Routes>
+        <Route exact path='/' children={<Home />} />
+        <Route path='/services' children={<Services />} />
+      </Routes> */}
+      
       <Footer />
     </Router>
   );
---
+++ website/src/components/pages/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ServicesList from '../ServicesList';
+function Services() {
+  return (
+    <>
+      <ServicesList />
+    </>
+  );
+}
+
+export default Services;
