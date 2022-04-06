cec93b26b1a0f58818c3d330a7dce59b35c2fccb
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-06 08:14:07 +0500

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,6 @@
 import { Link, Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/HomePage/Home';
-import HeroSection from './components/pages/HeroSection';
 import Navbar from './components/pages/Navbar';
 import Footer from './components/pages/Footer/Footer';
 import Services from './components/pages/Services/Services';
