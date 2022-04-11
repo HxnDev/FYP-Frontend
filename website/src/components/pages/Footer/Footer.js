16ec92a231170bb1517cef296b3aa88e7b25ea08
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-11 10:31:20 +0500

    Footer Updated

--- website/src/components/pages/Footer/Footer.js
+++ website/src/components/pages/Footer/Footer.js
@@ -11,72 +11,44 @@ import {
 function Footer() {
   return (
     <div className='footer-container'>
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Reviews</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Twitter</Link>
-          </div>
-        </div>
-      </div>
       <section className='social-media'>
+        <div className='social-icons'>
+          <Link
+            className='social-icon-link'
+            to='/'
+            target='_blank'
+            aria-label='Facebook'
+          >
+            <FaFacebook />
+          </Link>
+          <Link
+            className='social-icon-link'
+            to='/'
+            target='_blank'
+            aria-label='Instagram'
+          >
+            <FaInstagram />
+          </Link>
+          <Link
+            className='social-icon-link'
+            to='/'
+            target='_blank'
+            aria-label='Twitter'
+          >
+            <FaTwitter />
+          </Link>
+          <Link
+            className='social-icon-link'
+            to='/'
+            target='_blank'
+            aria-label='LinkedIn'
+          >
+            <FaLinkedin />
+          </Link>
+        </div>
+
         <div className='social-media-wrap'>
-          <div className='footer-logo'>
-          </div>
           <small className='website-rights'>THINK VISION © 2022</small>
-          <div className='social-icons'>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <FaFacebook />
-            </Link>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <FaInstagram />
-            </Link>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <FaTwitter />
-            </Link>
-            <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <FaLinkedin />
-            </Link>
-          </div>
         </div>
       </section>
     </div>
