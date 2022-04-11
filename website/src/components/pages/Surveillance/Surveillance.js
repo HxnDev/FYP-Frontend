1a8eb5a91ef206d6530341a8f5a6d9db37bde68a
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-11 14:16:18 +0500

    Surveillance feed added

--- website/src/components/pages/Surveillance/Surveillance.js
+++ website/src/components/pages/Surveillance/Surveillance.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Models from './Models';
-
+import Webcam from './Webcam';
 function Surveillance() {
   return (
     <>
+      <Webcam />
       <Models /> 
     </>
   );
---
+++ website/src/components/pages/Surveillance/Webcam.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Webcam.css';
+
+
+function Webcam() {
+  return (
+    <>
+
+        <div className='webcam-container'>
+            <header>
+                <img src='./images/svg-1.svg' className="webcam" alt="logo" />
+            </header>
+      </div>
+    </>
+  );
+}
+
+export default Webcam;
