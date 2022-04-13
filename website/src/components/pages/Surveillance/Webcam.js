259c52fc459b230ea8549600c1e18b30b07eff29
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-13 12:16:35 +0500

    Final Version

--- website/src/components/pages/Surveillance/Webcam.js
+++ website/src/components/pages/Surveillance/Webcam.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Webcam.css';
+import axios from 'axios';
 
 function Webcam() {
+  useEffect(()=>{
+    // this runs when react page loads
+    axios.get('http://localhost:5000/init').then(response => {
+      console.log("SUCCESS", response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
   return (
     <>
 
         <div className='webcam-container'>
             <header>
-                <img src='./images/ai-hand.png' className="webcam" alt="camera not readable" />
+                <img src={'http://127.0.0.1:5000/stream'} className="webcam" alt="camera not readable" />
             </header>
         </div>
     </>
