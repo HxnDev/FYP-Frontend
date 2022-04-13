5f60243e814d2bc75ee052a7cb236365df40e79c
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-13 10:04:36 +0500

    Images Updated

--- website/src/components/pages/HomePage/Data.js
+++ website/src/components/pages/HomePage/Data.js
@@ -1,7 +1,7 @@
 export const homeObjOne = {
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
     headline: 'Creating Smart Solutions',
     description:
       'At Think Vision, we take the power of Artificial Intelligence and Machine Learning and apply it to computer vision. Our advanced models can use whatever the camera sees and make judgements about objects, people, activities and more. Interested? Lets talk more about it.',
@@ -12,9 +12,9 @@ export const homeObjOne = {
   };
 
 export const homeObjTwo = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
   headline: 'Watching out for your ease and safety',
   description:
     'Manual surveillance is prone to human error and gaps in oversight. We can address these issues - our models will tirelessly watch and catch out anything suspicious. From aggressive behaviour to unauthorised people in restricted areas, our AI can address any vulnerabiiity, threat or risk.',
@@ -25,9 +25,9 @@ export const homeObjTwo = {
 };
 
 export const homeObjThree = {
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
   headline: 'Keeping an eye out for anything alarming',
   description:
     'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
